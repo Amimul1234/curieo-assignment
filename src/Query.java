cc2821eec62c412d38d8567433f3330ef1490f4e
amimulehsan <amimulehsan56@gmail.com>
2024-06-13 15:05:45 +0600

    Added query and transaction model in project.

--- src/Query.java
+++ src/Query.java
@@ -1,26 +1,2 @@
-public class Query {
-
-    private int startTime;
-    private int endTime;
-
-    public Query(int startTime, int endTime) {
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    public int getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(int startTime) {
-        this.startTime = startTime;
-    }
-
-    public int getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(int endTime) {
-        this.endTime = endTime;
-    }
+public record Query(int startTime, int endTime) {
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,24 +1,2 @@
-public class Transaction {
-
-    private final int amount;
-    private final String type;
-    private final int timestamp;
-
-    public Transaction(int timestamp, String type, int amount) {
-        this.timestamp = timestamp;
-        this.type = type;
-        this.amount = amount;
-    }
-
-    public int getTimestamp() {
-        return timestamp;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
+public record Transaction(int timestamp, TransactionType type, int amount) {
 }
